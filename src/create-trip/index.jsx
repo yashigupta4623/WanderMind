1c5542fe112053d0990d1415afd6615eff6c54fb
Yashi Gupta <yashig406@gmail.com>
2025-01-18 19:32:17 +0530

    Google Authentication

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,23 +1,56 @@
-import React, { useState } from 'react';
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import React, { useState, useEffect } from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 import { Input } from "@/components/ui/input";
 import { SelectBudgetOptions, SelectTravelsList } from "@/constants/options";
 import { Button } from "@/components/ui/button";
-import { toast } from 'sonner';
-import { chatSession } from '@/service/AIModal';
+import { toast } from "sonner";
+import { chatSession } from "@/service/AIModal";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { FcGoogle } from "react-icons/fc";
+import { useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 
 const apiKey = import.meta.env.VITE_GOOGLE_PLACE_API_KEY;
 
 function CreateTrip() {
   const [place, setPlace] = useState(null);
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState([]);
+  const [openDialog, setOpenDialog] = useState(false);
 
-  const handleInputChange = (field, value) => {
-    setFormData(prevState => ({ ...prevState, [field]: value }));
+  const handleInputChange = (name, value) => {
+    setFormData({ ...formData, [name]: value });
   };
 
-  const OnGenerateTrip =async() => {
-    if (!formData?.location || !formData?.noofDays || !formData?.budget || !formData?.traveler) {
+  useEffect(() => {
+    console.log(formData);
+  }, [formData]);
+
+  const login = useGoogleLogin({
+    onSuccess: (tokenResponse) => GetUserProfile(tokenResponse),
+    onError: (error) => console.log(error),
+  });
+
+  const OnGenerateTrip = async () => {
+    
+    const user = localStorage.getItem("user");
+    if (!user) {
+      setOpenDialog(true);
+      toast("Please login to generate trip");
+    }
+
+    if (
+      !formData?.location ||
+      !formData?.noofDays ||
+      !formData?.budget ||
+      !formData?.traveler
+    ) {
       toast("Please fill all the details");
       return;
     }
@@ -30,69 +63,135 @@ function CreateTrip() {
     console.log(result?.response?.text());
   };
 
-  return (
-    <div className='sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10'>
-      <h2 className='font-bold text-3xl'>Tell us your travel preferences 🏕️</h2>
-      <p className='mt-3 text-gray-500'>Just provide some basic information, and we'll help you plan your trip.</p>
+  const GetUserProfile = (tokenInfo) => {
+    axios
+      .get(
+        `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${tokenInfo.access_token}`,
+        {
+          headers: {
+            Authorization: `Bearer ${tokenInfo.access_token}`,
+            Accept: "application/json",
+          },
+        }
+      )
+      .then((resp) => {
+        console.log(resp.data);
+        localStorage.setItem("user", JSON.stringify(resp.data));
+        setOpenDialog(false);
+        OnGenerateTrip();
+      })
+  };
 
-      <div className='mt-10 flex flex-col gap-5'>
+  return (
+    <div className="sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10">
+      <h2 className="font-bold text-3xl">Tell us your travel preferences 🏕️</h2>
+      <p className="mt-3 text-gray-500">
+        Just provide some basic information, and we'll help you plan your trip.
+      </p>
+
+      <div className="mt-10 flex flex-col gap-5">
         <div>
-          <h2 className='text-xl my-3 font-medium'>What is your destination of choice?</h2>
+          <h2 className="text-xl my-3 font-medium">
+            What is your destination of choice?
+          </h2>
           <GooglePlacesAutocomplete
             apiKey={apiKey}
             selectProps={{
-              placeholder: 'Search for places...',
+              placeholder: "Search for places...",
               value: place,
-              onChange: (v) => { setPlace(v); handleInputChange('location', v); }
+              onChange: (v) => {
+                setPlace(v);
+                handleInputChange("location", v);
+              },
             }}
           />
         </div>
 
         <div>
-          <h2 className='text-xl my-3 font-medium'>How many days are you planning your trip?</h2>
-          <Input 
-            placeholder='Number of days...' 
-            type='number' 
-            onChange={(e) => handleInputChange('noofDays', e.target.value)} 
+          <h2 className="text-xl my-3 font-medium">
+            How many days are you planning your trip?
+          </h2>
+          <Input
+            placeholder="Number of days..."
+            type="number"
+            onChange={(e) => handleInputChange("noofDays", e.target.value)}
           />
         </div>
       </div>
 
       <div>
-        <h2 className='text-xl my-3 font-medium'>What is your Budget?</h2>
-        <div className='grid grid-cols-3 gap-5 mt-5'>
+        <h2 className="text-xl my-3 font-medium">What is your Budget?</h2>
+        <div className="grid grid-cols-3 gap-5 mt-5">
           {SelectBudgetOptions.map((item, index) => (
-            <div key={index} onClick={() => handleInputChange('budget', item.title)}
-            className={`p-4 border rounded-lg hover:shadow-lg cursor-pointer
-            ${formData?.budget === item.title && 'shadow-lg border-black'}`}>
-              <h2 className='text-4xl'>{item.icon}</h2>
-              <h2 className='font-bold text-lg'>{item.title}</h2>
-              <h2 className='text-sm text-gray-500'>{item.desc}</h2>
+            <div
+              key={index}
+              onClick={() => handleInputChange("budget", item.title)}
+              className={`p-4 border rounded-lg hover:shadow-lg cursor-pointer
+            ${formData?.budget === item.title && "shadow-lg border-black"}`}
+            >
+              <h2 className="text-4xl">{item.icon}</h2>
+              <h2 className="font-bold text-lg">{item.title}</h2>
+              <h2 className="text-sm text-gray-500">{item.desc}</h2>
             </div>
           ))}
         </div>
       </div>
-      
+
       <div>
-        <h2 className='text-xl my-3 font-medium'>Who do you plan on travelling with on your next adventure?</h2>
-        <div className='grid grid-cols-4 gap-5 mt-5'>
+        <h2 className="text-xl my-3 font-medium">
+          Who do you plan on travelling with on your next adventure?
+        </h2>
+        <div className="grid grid-cols-4 gap-5 mt-5">
           {SelectTravelsList.map((item, index) => (
-            <div key={index} onClick={() => handleInputChange('traveler', item.people)}
-            className={`p-4 border rounded-lg hover:shadow-lg cursor-pointer
-              ${formData?.traveler === item.people && 'shadow-lg border-black'}`}>
-              <h2 className='text-4xl'>{item.icon}</h2>
-              <h2 className='font-bold text-lg'>{item.title}</h2>
-              <h2 className='text-sm text-gray-500'>{item.desc}</h2>
+            <div
+              key={index}
+              onClick={() => handleInputChange("traveler", item.people)}
+              className={`p-4 border rounded-lg hover:shadow-lg cursor-pointer
+              ${
+                formData?.traveler === item.people && "shadow-lg border-black"
+              }`}
+            >
+              <h2 className="text-4xl">{item.icon}</h2>
+              <h2 className="font-bold text-lg">{item.title}</h2>
+              <h2 className="text-sm text-gray-500">{item.desc}</h2>
             </div>
           ))}
         </div>
       </div>
 
-      <div className='my-10 justify-end flex '>
-      <Button onClick={OnGenerateTrip}>Generate Trip</Button>
+      <div className="my-10 justify-end flex ">
+        <Button onClick={OnGenerateTrip}>Generate Trip</Button>
       </div>
 
-    </div >
+      <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogDescription>
+            <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+  <img 
+    src="./public/logo.svg" 
+    alt="logo" 
+    style={{ height: "40px", width: "50px" }} 
+  />
+  <h2 style={{ fontWeight: "bold", color: "#f56551", fontSize: "25px" }}>
+    Wander<span className="text-[#000000]">Mind</span>
+  </h2>
+</div>
+
+              <h2 className="font-bold text-lg mt-5">Sign In With Google</h2>
+              <p>Sign in to the App with the Google authentication sexure.</p>
+              <Button
+                onClick={login}
+                className="mt-5 w-full flex gap-4 items-center"
+              >
+                <FcGoogle style={{ height: "35px", width: "25px" }} />
+                Sign In With Google
+              </Button>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import CreateTrip from './create-trip/index.jsx'
 import Header from './components/custom/Header.jsx'
 import { Toaster } from 'sonner'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const router = createBrowserRouter([
   {
@@ -20,8 +21,10 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
     <Header/>
     <Toaster />
    <RouterProvider router={router} />
+   </GoogleOAuthProvider>;
   </StrictMode>,
 )
