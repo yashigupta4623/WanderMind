a8e748ebbb6376cc03be66ad410c351694b8dbbf
Yashi Gupta <98335217+yashigupta4623@users.noreply.github.com>
2025-09-20 14:38:35 +0530

    change the gemini api model form 1.5 to 2.0

--- src/service/AIModel.jsx
+++ src/service/AIModel.jsx
@@ -4,7 +4,7 @@ const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
+  model: "gemini-2.0-flash",
 });
 
 const generationConfig = {
